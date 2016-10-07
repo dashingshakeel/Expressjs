e878ec54baf35613ca158a58a2f26a8dca5840c2
dashingshakeel <shkeelrauf@gmail.com>
2016-10-07 19:59:48 +0500

    Learned how to routes

---
+++ server2.js
@@ -0,0 +1,30 @@
+var express= require('express');
+var app= express();
+var bodyParser=require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+var names = [];
+app.all('/',function(req,res,next){
+  console.log('from all');
+  next();
+});
+
+app.get('/', function(req,res,next){
+  console.log(names);
+  next();
+},function(req,res){
+  res.render('index.jade',{names: names})
+});
+
+app.post('/',function(req,res){
+  names.push(req.body.name);
+  console.log(names[2]);
+  res.redirect('/');
+});
+
+app.listen(3000);
